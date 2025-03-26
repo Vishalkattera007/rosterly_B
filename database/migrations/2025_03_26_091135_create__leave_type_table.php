2e550fa72b4a928092e602529fb22fc3cff88f22
Glansa-Solutions <glansa.solutions777@gmail.com>
2025-03-26 16:00:35 +0530

    leave_type

---
+++ database/migrations/2025_03_26_091135_create__leave_type_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leave_type', function (Blueprint $table) {
+            $table->id();
+            $table->string('leave_type')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable(); // User who created the record
+            $table->unsignedBigInteger('updated_by')->nullable(); // User who last updated the record
+            $table->timestamps();
+            $table->enum('status', ['active', 'inactive'])->default('active'); // ENUM with default 'active'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_type');
+    }
+};
