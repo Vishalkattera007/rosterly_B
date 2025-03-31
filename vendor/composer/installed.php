21b684eaddf6b4a21f2d34c7d104f7f92838007e
SouravB10 <souravbehuria10@gmail.com>
2025-03-31 12:28:16 +0530

    regerre

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.0.3',
-        'version' => '12.0.3.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '532eb726ccf697da3cdce0afaa3933ea11e6ee93',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.0.3',
-            'version' => '12.0.3.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '532eb726ccf697da3cdce0afaa3933ea11e6ee93',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
